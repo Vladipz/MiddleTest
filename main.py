5f9ca1866f480e647d8a0972626ef811882f8321
Vlad Danilchuk <vladdanilchuk9@gmail.com>
2024-04-03 09:34:45 +0300

    add firt try methods

---
+++ main.py
@@ -0,0 +1,43 @@
+
+def read_file(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            text = file.read()
+            return text
+    except FileNotFoundError:
+        print("File not found.")
+        return None
+
+def count_words(text):
+    separators = [' ', ',', ':', ';']  # роздільники слів
+    for sep in separators:
+        text = text.replace(sep, ' ')  # замінюємо роздільники пробілами
+    words = text.split()  # Розділяємо текст на слова за пробілами
+    return len(words)
+
+def count_sentences(text):
+    sentences = text.split('.')  
+    sentences += text.split('!')  
+    sentences += text.split('?') 
+    sentences += text.split('...') 
+    return len(sentences)
+
+def analyze_text(file_path):
+    text = read_file(file_path)
+    if text:
+        num_words = count_words(text)
+        num_sentences = count_sentences(text)
+        return num_words, num_sentences
+    else:
+        return None, None
+
+# Приклад використання:
+if __name__ == "__main__":
+    file_path = "test.txt"  # Вкажіть шлях до свого файлу
+    num_words, num_sentences = analyze_text(file_path)
+    if num_words is not None and num_sentences is not None:
+        print("Кількість слів у тексті:", num_words)
+        print("Кількість речень у тексті:", num_sentences)
+
+
+
